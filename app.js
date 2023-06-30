ad1622aebdf95a3e8498a86542ea4095bb1b4d31
efrainLllontop <Carlosefrainchallo1@gmail.com>
2023-06-30 00:02:24 -0500

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+const datosPersonas=[
+    new Persona('Juan','Llontop'),
+    new Persona('Mariela','Llontop')
+]
+function mostrarPersona() {
+    let nombreCompleto=' '
+    for(let persona of datosPersonas){
+        nombreCompleto+=`<li>${persona.nombre} ${persona.apellido}</li>`
+    }
+    document.getElementById('datoPersona').innerHTML=nombreCompleto
+}
+function agregarPersona() {
+    const forma=document.forms['formulario']
+    const nombre=forma['nombre']
+    const apellido=forma['apellido']
+    if (nombre.value!='' && apellido.value!='') {
+        const persona=new Persona(nombre.value, apellido.value)
+        datosPersonas.push(persona)
+        mostrarPersona()
+    } else {
+        console.log('No hay información por agregar');
+    }
+}
